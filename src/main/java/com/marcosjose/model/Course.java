9de1c5576bdd376f7e4501de086ed47f7fba0960
Marcos José de Oliveira <marcosjose000@gmail.com>
2022-11-02 15:53:27 -0300

    aula 15 concluida

---
+++ src/main/java/com/marcosjose/model/Course.java
@@ -0,0 +1,25 @@
+package com.marcosjose.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Course {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String name;
+
+    @Column(length = 50, nullable = false)
+    private String category;
+
+}
